a2a33033ac2c12fca2d197ffe52fa9f5510df4b8
Jack Wu <xuwupeng2000@gmail.com>
2015-03-14 18:26:47 +1300

    Fix topnav font color
    DGB-8

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,7 +1,13 @@
 class Admin::ProductsController <  Admin::BaseController
 
   def index
-
+    unless params[:filter].blank?
+      key_word = "%#{ params[:filter] }%"
+      scope = Product.where{( name.like key_word ) | ( id == key_word ) | ( description.like key_word )}
+    else
+      scope = Product.all
+    end
+    @products = smart_listing_create(:products, scope, partial: "admin/products/listing")
   end
 
   def update
