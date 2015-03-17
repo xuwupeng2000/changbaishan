5d7f0542038bce8b3a09885c764a42116b54b501
Jack Wu <xuwupeng2000@gmail.com>
2015-03-17 16:34:54 +1300

    Add bower to manage pure JS libs, add couple libs to use
      We are going to do AngularJS inline edting on user:: manage customer page
      So we need to get some ammo.
    
    DGB-2

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Changbaishan
   class Application < Rails::Application
+    config.assets.paths << Rails.root.join("vendor","assets","bower_components")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
